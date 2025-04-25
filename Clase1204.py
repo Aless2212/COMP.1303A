35d8bcea236a676221b7b5a134edb602bb63038d
Aless2212 <ppecerossaavedra@gmail.com>
2025-04-25 17:17:18 -0500

    Segundo commit

--- Clase1204.py
+++ Clase1204.py
@@ -1,11 +1,31 @@
+import random
+
 class Perro:
-    def __init__(self, nombre = " ", color = " ", edad = 0):
+
+    def __init__(self, nombre = "", color = "", edad = 0):
         self._nombre = nombre
         self._color = color
         self._edad = edad
+
+
+    def ladrar(self):
+        print("Guau")
+
+    
+    def saludo(self):
+        return "Pata" if random.randint(0,1) == 0 else "Cola"
+
+    def __str__(self):
+        return f"Perro: {self._nombre}, de color {self._color} y {self._edad} años"
+
         
-p = Perro()
-p._nombre = "Box"
-print(p._nombre)
-q = Perro("Matias", "Caramelo", 4)
+
+p = Perro("Fido", "Marron", 5)
+p.ladrar()
+print(f"Me saludo con la {p.saludo()}")
+print(p)
+print()
+
+q = Perro()
+p._nombre = "Rambo"
 print(q._nombre)
